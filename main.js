9b50545f157968eab6bcee767df38070a88f39be
Cristian Sarmiento <cristian.sarmiento@sofka.com.co>
2021-12-22 14:11:50 -0500

    Codigo practica MVC2

--- main.js
+++ main.js
@@ -6,16 +6,39 @@
     this.playing = false;
     this.game_over = false;
     this.bars = [];
-    this.bars = null;
+    this.ball = null;
 }
 
 self.Board.prototype = {
     get elements (){
         let elements = this.bars;
-        elements.push(ball);
+        elements.push(this.ball);
         return elements;
     }
 }
+})();
+
+(function(){
+self.Bar = function(x,y,width,height,board){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.board = board;
+    this.board.bars.push(this);
+    this.kind = "rectangle";
+    console.log('entra al constructor')
+}
+
+self.Bar.prototype = {
+    down: function(){
+
+    },
+    up: function(){
+
+    }
+}
+
 })();
 
 (function (){
@@ -26,12 +49,43 @@ self.BoardView = function(canvas, board){
     this.board = board;
     this.ctx = canvas.getContext("2d");
 }
+
+self.BoardView.prototype = { 
+    draw: function (){
+        for (let i = this.board.elements.length - 1; i >=0; i--){
+            let el = this.board.elements[i];
+            // console.log(el)
+            draw(this.ctx, el);
+        };
+    }
+}
+
+function draw(ctx, element){
+    // console.log('holas', element)
+    if(element !== null && element.hasOwnProperty("kind")){
+       
+        switch(element.kind){
+            case "rectangle":
+                // console.log('prueba final')
+                // console.log(element.x, element.y, element.width, element.height)
+                ctx.fillRect(element.x, element.y, element.width, element.height);
+                break;
+
+    }
+    }
+}
+
 })();
 
 window.addEventListener("load", main);
 
 function main(){
+console.log('Hola mundo')
 let board = new Board(800, 400);
+let bar = new Bar(20,100,40,100,board);
+let bar2 = new Bar(735,100,40,100,board);
 let canvas = document.getElementById('canvas');
 let board_view = new BoardView(canvas, board);
+console.log(board);
+board_view.draw();
 }
